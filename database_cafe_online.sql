89048c2f5919edd1a60d6901694c846fe55df91d
Tran Thanh Son <sonva2002@gmail.com>
2023-05-27 08:00:42 +0700

    thanh son up table user

--- database_cafe_online.sql
+++ database_cafe_online.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE shop_cafe_online;
 USE shop_cafe_online;
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(50) NOT NULL,
+    pass_word VARCHAR(50) NOT NULL UNIQUE,
+    Email VARCHAR(50) NOT NULL UNIQUE,
+    phone VARCHAR(10)NOT NULL 
+);
