14f58588cc8a2274f071c1721a6b365fb3e76abb
cao <vietcao2510@gmail.com>
2023-05-27 08:06:27 +0700

    Cao upDate Product

--- database_cafe_online.sql
+++ database_cafe_online.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE shop_cafe_online;
 USE shop_cafe_online;
+create table Product(
+ProductID varchar(45) primary key ,
+ProductName varchar(45),
+`Description` varchar(100),
+Price double,
+Stock int
+); 
