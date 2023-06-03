f2a444f32539d2c52dd08961298b078a794f1b0e
TranDinhNgocSang <sangdolphin98@gmail.com>
2023-06-03 11:06:10 +0700

    SANG CM 6/62023

---
+++ coffee_shop/src/main/java/com/example/coffee_shop/BaseRepository.java
@@ -0,0 +1,26 @@
+package com.example.coffee_shop;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseRepository {
+    private String jdbcURL = "jdbc:mysql://localhost:3306/shop_cafe_online";
+    private String jdbcUsername = "root";
+    private String jdbcPassword = "codegym";
+
+    protected Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+}
