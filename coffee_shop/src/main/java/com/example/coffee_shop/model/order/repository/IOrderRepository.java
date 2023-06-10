09ee7337012bed2d0618edfbef4f692b607c585b
Tran Thanh Son <sonva2002@gmail.com>
2023-06-10 14:44:24 +0700

    son update database lan cuoi

--- coffee_shop/src/main/java/com/example/coffee_shop/model/order/repository/IOrderRepository.java
+++ coffee_shop/src/main/java/com/example/coffee_shop/model/order/repository/IOrderRepository.java
@@ -2,12 +2,14 @@ package com.example.coffee_shop.model.order.repository;
 
 import com.example.coffee_shop.model.order.model.DetailOrder;
 import com.example.coffee_shop.model.order.model.Order;
+import com.example.coffee_shop.model.order.model.StatusOrder;
 
 import java.util.List;
 
 public interface IOrderRepository{
     //display
     List<Order> displayOrder();
+    List<StatusOrder> displayStatus();
     //add
 
     //edit
--- coffee_shop/src/main/java/com/example/coffee_shop/model/order/service/IOrderService.java
+++ coffee_shop/src/main/java/com/example/coffee_shop/model/order/service/IOrderService.java
@@ -2,12 +2,14 @@ package com.example.coffee_shop.model.order.service;
 
 import com.example.coffee_shop.model.order.model.DetailOrder;
 import com.example.coffee_shop.model.order.model.Order;
+import com.example.coffee_shop.model.order.model.StatusOrder;
 
 import java.util.List;
 
 public interface IOrderService {
     //display
     List<Order> displayOrder();
+    List<StatusOrder> displayStatus();
     //add
 
     //edit
